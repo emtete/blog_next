b63a39e40cf57f221c6d6e27f7ed04f087557abd
최종호 <victor_77@naver.com>
2020-08-15 13:07:37 +0900

    update server side rendering by refference material-ul sample code

--- front/pages/_app.jsx
+++ front/pages/_app.jsx
@@ -1,22 +1,40 @@
 import React from "react";
-import Head from "next/head";
 import PropTypes from "prop-types";
-// import "antd/dist/antd.css";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
-// import wrapper from "../store/configureStore";
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-const NodeBird = ({ Component }) => (
-  <>
-    <Head>
-      <title>NodeBird</title>
-      {/* <link rel='shortcut icon' href='/favicon.ico' /> */}
-    </Head>
-    <Component />
-  </>
-);
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-NodeBird.propTypes = {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 };
-
-export default NodeBird;
