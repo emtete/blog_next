90ad0d4404ba9eb920f91324233446774f523bf1
최종호 <victor_77@naver.com>
2020-08-17 16:02:08 +0900

    apply draft.js and working

---
+++ front/components/Wyzywig.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { EditorState, convertToRaw } from "draft-js";
+import dynamic from "next/dynamic";
+import draftToHtml from "draftjs-to-html";
+
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
+  { ssr: false }
+);
+
+const htmlToDraft = dynamic(
+  () => import("html-to-draftjs").then((mod) => mod.htmlToDraft),
+  { ssr: false }
+);
+
+// const draftToHtml = dynamic(
+//   () => import("draftjs-to-html").then((mod) => mod.draftToHtml),
+//   { ssr: false }
+// );
+
+class Wyzywig extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div>
+        <Editor
+          editorState={editorState}
+          wrapperClassName='demo-wrapper'
+          editorClassName='demo-editor'
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </div>
+    );
+  }
+}
+
+export default Wyzywig;
--- front/pages/board.jsx
+++ front/pages/board.jsx
@@ -1,44 +1,16 @@
 import dynamic from "next/dynamic";
-// import {con} from "draftjs-to-html";
-// import htmlToDraft from "html-to-draftjs";
-// import { draftToHtml } from "react-draft-wysiwyg";
-
-const Editor = dynamic(
-  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
-  { ssr: false }
-);
+import { useEffect, useState } from "react";
 
 // Dependency Component
 import AppLayout from "../components/AppLayout";
-import { useState } from "react";
+import Wyzywig from "../components/wyzywig";
 
 const Board = () => {
-  const [editorState, setEditorState] = useState();
-
-  const onEditorStateChange = (editorState) => {
-    setEditorState({
-      editorState,
-    });
-  };
-
-  return (
-    <AppLayout>
-      <div>
-        <Editor
-          editorState={editorState}
-          wrapperClassName='demo-wrapper'
-          editorClassName='demo-editor'
-          onEditorStateChange={onEditorStateChange}
-        />
-        <textarea
-          disabled
-          // value={Editor.draftToHtml(
-          //   Editor.convertToRaw(editorState.getCurrentContent())
-          // )}
-        />
-      </div>
-    </AppLayout>
-  );
+  // const [Wyzysig, setWyzysig] = useState();
+  // useEffect(() => {
+  //   setWyzysig(import("../components/Wyzywig"));
+  // });
+  return <AppLayout>{Wyzywig ? <Wyzywig /> : <div>loading</div>}</AppLayout>;
 };
 
 export default Board;
