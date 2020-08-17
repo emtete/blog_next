1eb5ce523e65b08c384a7c03b6b8271877f2ab46
최종호 <victor_77@naver.com>
2020-08-17 14:05:32 +0900

    apply draft and not working..

--- front/pages/board.jsx
+++ front/pages/board.jsx
@@ -1,5 +1,8 @@
-// import { Editor } from "react-draft-wysiwyg";
 import dynamic from "next/dynamic";
+// import {con} from "draftjs-to-html";
+// import htmlToDraft from "html-to-draftjs";
+// import { draftToHtml } from "react-draft-wysiwyg";
+
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
   { ssr: false }
@@ -7,17 +10,33 @@ const Editor = dynamic(
 
 // Dependency Component
 import AppLayout from "../components/AppLayout";
+import { useState } from "react";
 
 const Board = () => {
+  const [editorState, setEditorState] = useState();
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState({
+      editorState,
+    });
+  };
+
   return (
     <AppLayout>
-      <Editor
-        // editorState={editorState}
-        toolbarClassName='toolbarClassName'
-        wrapperClassName='wrapperClassName'
-        editorClassName='editorClassName'
-        // onEditorStateChange={this.onEditorStateChange}
-      />
+      <div>
+        <Editor
+          editorState={editorState}
+          wrapperClassName='demo-wrapper'
+          editorClassName='demo-editor'
+          onEditorStateChange={onEditorStateChange}
+        />
+        <textarea
+          disabled
+          // value={Editor.draftToHtml(
+          //   Editor.convertToRaw(editorState.getCurrentContent())
+          // )}
+        />
+      </div>
     </AppLayout>
   );
 };
