ce654deb287668b5b3e4ea230dd4d91bcd01c25e
최종호 <victor_77@naver.com>
2020-08-17 14:47:23 +0900

    apply quill but not working

--- front/pages/board.jsx
+++ front/pages/board.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect, useRef } from "react";
+
 import AppLayout from "../components/AppLayout";
 
 const Board = () => {
+  const [value, setValue] = useState();
+  const ReactQuill =
+    typeof window === "object" ? require("react-quill") : () => false;
+
+  const handleEditor = (value) => {
+    setValue(value);
+  };
+
   return (
     <AppLayout>
       <div>Board</div>
+      <ReactQuill value={value} onChange={handleEditor} theme={"snow"} />
     </AppLayout>
   );
 };
