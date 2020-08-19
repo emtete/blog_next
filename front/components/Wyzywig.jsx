6e7aa97ac0f61f84932bc76213167231eca52797
최종호 <victor_77@naver.com>
2020-08-19 17:36:40 +0900

    add button to wyzywig component

--- front/components/Wyzywig.jsx
+++ front/components/Wyzywig.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import dynamic from "next/dynamic";
 import draftToHtml from "draftjs-to-html";
-import { TextField, FormControl } from "@material-ui/core";
+import { TextField, FormControl, Button } from "@material-ui/core";
 
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
@@ -28,34 +28,49 @@ class Wyzywig extends Component {
   render() {
     const { editorState } = this.state;
     return (
-      <div>
-        <FormControl>
-          <TextField id='title' label='title' />
-          <br />
-          <Editor
-            editorState={editorState}
-            wrapperClassName='demo-wrapper'
-            editorClassName='demo-editor'
-            editorStyle={{
-              height: "275px",
-              borderWidth: "1px",
-              borderStyle: "solid",
-              borderColor: "rgb(241, 241, 241)",
-              borderImage: "initial",
-              padding: "5px",
-              borderRadius: "2px",
-            }}
-            onEditorStateChange={this.onEditorStateChange}
-            localization={{
-              locale: "ko",
-            }}
-          />
-          <textarea
+      <FormControl>
+        <TextField id='title' label='title' />
+        <br />
+        <Editor
+          editorState={editorState}
+          wrapperClassName='demo-wrapper'
+          editorClassName='demo-editor'
+          editorStyle={{
+            height: "275px",
+            borderWidth: "1px",
+            borderStyle: "solid",
+            borderColor: "rgb(241, 241, 241)",
+            borderImage: "initial",
+            padding: "5px",
+            borderRadius: "2px",
+          }}
+          onEditorStateChange={this.onEditorStateChange}
+          localization={{
+            locale: "ko",
+          }}
+        />
+        {/* <textarea
             disabled
             value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-          />
-        </FormControl>
-      </div>
+          /> */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            marginTop: "10px",
+          }}
+        >
+          <Button
+            variant='contained'
+            color='primary'
+            type='submit'
+            fullWidth={false}
+            size='large'
+          >
+            등록
+          </Button>
+        </div>
+      </FormControl>
     );
   }
 }
