13d4f204f3130d098128ee4006d6b9d2997036a3
최종호 <victor_77@naver.com>
2020-08-19 17:21:23 +0900

    add textFiled to Wyzywig component

--- front/components/Wyzywig.jsx
+++ front/components/Wyzywig.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import dynamic from "next/dynamic";
 import draftToHtml from "draftjs-to-html";
+import { TextField, FormControl } from "@material-ui/core";
 
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
@@ -28,28 +29,32 @@ class Wyzywig extends Component {
     const { editorState } = this.state;
     return (
       <div>
-        <Editor
-          editorState={editorState}
-          wrapperClassName='demo-wrapper'
-          editorClassName='demo-editor'
-          editorStyle={{
-            height: "275px",
-            borderWidth: "1px",
-            borderStyle: "solid",
-            borderColor: "rgb(241, 241, 241)",
-            borderImage: "initial",
-            padding: "5px",
-            borderRadius: "2px",
-          }}
-          onEditorStateChange={this.onEditorStateChange}
-          localization={{
-            locale: "ko",
-          }}
-        />
-        <textarea
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-        />
+        <FormControl>
+          <TextField id='title' label='title' />
+          <br />
+          <Editor
+            editorState={editorState}
+            wrapperClassName='demo-wrapper'
+            editorClassName='demo-editor'
+            editorStyle={{
+              height: "275px",
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderColor: "rgb(241, 241, 241)",
+              borderImage: "initial",
+              padding: "5px",
+              borderRadius: "2px",
+            }}
+            onEditorStateChange={this.onEditorStateChange}
+            localization={{
+              locale: "ko",
+            }}
+          />
+          <textarea
+            disabled
+            value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+          />
+        </FormControl>
       </div>
     );
   }
