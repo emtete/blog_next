99bb6fbc3583d430a158177ed5424723c57fc1eb
최종호 <victor_77@naver.com>
2020-08-20 16:30:44 +0900

    update menu setting list to TreeView

--- front/components/SettingTabs.jsx
+++ front/components/SettingTabs.jsx
@@ -15,21 +15,23 @@ import { Divider } from "@material-ui/core";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+  // return (
+  //   <div
+  //     role='tabpanel'
+  //     hidden={value !== index}
+  //     id={`vertical-tabpanel-${index}`}
+  //     aria-labelledby={`vertical-tab-${index}`}
+  //     {...other}
+  //   >
+  //     {value === index && (
+  //       <Box p={5}>
+  //         <Typography>{children}</Typography>
+  //       </Box>
+  //     )}
+  //   </div>
+  // );
+
+  return <div>{value === index && <div>{children}</div>}</div>;
 }
 
 TabPanel.propTypes = {
--- front/components/Settings.jsx
+++ front/components/Settings.jsx
@@ -1,145 +1,60 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Checkbox from "@material-ui/core/Checkbox";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
-import SendIcon from "@material-ui/icons/Send";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
-import StarBorder from "@material-ui/icons/StarBorder";
-import DeleteIcon from "@material-ui/icons/Delete";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import IconButton from "@material-ui/core/IconButton";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { TreeItem } from "@material-ui/lab";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: "#424242",
-    color: "#ffffff",
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-  checkbox: {
-    root: {
-      color: "#ffffff",
+const data = {
+  id: "root",
+  name: "Parent",
+  children: [
+    {
+      id: "1",
+      name: "Child - 1",
     },
+    {
+      id: "3",
+      name: "Child - 3",
+      children: [
+        {
+          id: "4",
+          name: "Child - 4",
+        },
+      ],
+    },
+  ],
+};
+
+const useStyles = makeStyles({
+  root: {
+    height: 110,
+    flexGrow: 1,
+    maxWidth: 400,
   },
-}));
+});
 
-export default function settings() {
+export default function Settings() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const [checked, setChecked] = React.useState([0]);
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
-  // const labelId = `checkbox-list-label-${value}`;
-  const labelId = `checkbox-list-label-1`;
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <List
-        component='nav'
-        aria-labelledby='nested-list-subheader'
-        subheader={
-          <ListSubheader component='div' id='nested-list-subheader'>
-            Nested List Items
-          </ListSubheader>
-        }
+    <div style={{ width: "300px" }}>
+      <TreeView
         className={classes.root}
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpanded={["root"]}
+        defaultExpandIcon={<ChevronRightIcon />}
       >
-        {/* <ListItem button> */}
-        {/* key={value} */}
-        <ListItem role={undefined} dense button onClick={handleToggle(1)}>
-          <ListItemIcon>
-            <Checkbox
-              edge='start'
-              checked={checked.indexOf(1) !== -1}
-              tabIndex={-1}
-              disableRipple
-              inputProps={{ "aria-labelledby": labelId }}
-              style={{ color: "#ffffff" }}
-            />
-          </ListItemIcon>
-          <ListItemText primary='Sent mail' />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText primary='Drafts' />
-        </ListItem>
-        <ListItem button selected={true}>
-          <ListItemIcon>
-            <InboxIcon />
-            {/* <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction> */}
-          </ListItemIcon>
-          {/* <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction> */}
-          <ListItemText primary='Inbox' />
-          {open ? (
-            <ListItemSecondaryAction>
-              <IconButton edge='end' aria-label='expand'>
-                <ExpandLess
-                  button
-                  onClick={handleClick}
-                  style={{ color: "#ffffff" }}
-                />
-              </IconButton>
-            </ListItemSecondaryAction>
-          ) : (
-            <ListItemSecondaryAction>
-              <IconButton edge='end' aria-label='expand'>
-                <ExpandMore
-                  button
-                  onClick={handleClick}
-                  style={{ color: "#ffffff" }}
-                />
-              </IconButton>
-            </ListItemSecondaryAction>
-          )}
-        </ListItem>
-        <Collapse in={open} timeout='auto' unmountOnExit>
-          <List component='div' disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary='Starred' />
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
+        {renderTree(data)}
+      </TreeView>
     </div>
   );
 }
