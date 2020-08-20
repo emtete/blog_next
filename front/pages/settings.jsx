7f61aa8ebdc0f6bbda9fbcec41454fd50aac13f6
최종호 <victor_77@naver.com>
2020-08-20 15:11:44 +0900

    add setting tabs

--- front/pages/settings.jsx
+++ front/pages/settings.jsx
@@ -1,12 +1,15 @@
 import AppLayout from "../components/AppLayout";
 import Settings from "../components/Settings";
+import SettingTabs from "../components/SettingTabs";
 
 const settings = () => {
   return (
     <AppLayout>
-      <Settings>
-        <div>abc</div>
-      </Settings>
+      <SettingTabs>
+        <Settings>
+          <div>abc</div>
+        </Settings>
+      </SettingTabs>
     </AppLayout>
   );
 };
