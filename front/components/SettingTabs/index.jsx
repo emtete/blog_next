ca1c4808ab4b84a66b131970a481d4863c693577
최종호 <victor_77@naver.com>
2020-09-03 14:28:24 +0900

    apply sortableTree

--- front/components/SettingTabs/index.jsx
+++ front/components/SettingTabs/index.jsx
@@ -1,80 +1,25 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
-import SendIcon from "@material-ui/icons/Send";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
-import StarBorder from "@material-ui/icons/StarBorder";
+import React, { Component } from "react";
+import SortableTree from "react-sortable-tree";
+// import { SortableTreeWithoutDndContext as SortableTree } from "react-sortable-tree";
+// This only needs to be imported once in your app
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    marginTop: "10%",
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}));
+export default class SettingTabs extends Component {
+  constructor(props) {
+    super(props);
 
-const SettingTabs = () => {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+    this.state = {
+      treeData: [{ title: "Chicken", children: [{ title: "Egg" }] }],
+    };
+  }
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
-  return (
-    <List
-      component='nav'
-      aria-labelledby='nested-list-subheader'
-      subheader={
-        <ListSubheader component='div' id='nested-list-subheader'>
-          메뉴관리
-        </ListSubheader>
-      }
-      className={classes.root}
-    >
-      <ListItem button>
-        <ListItemIcon>
-          <SendIcon />
-        </ListItemIcon>
-        <ListItemText primary='Sent mail' />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <DraftsIcon />
-        </ListItemIcon>
-        <ListItemText primary='Drafts' />
-      </ListItem>
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary='Inbox' />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={open} timeout='auto' unmountOnExit>
-        <List component='div' disablePadding>
-          <ListItem button className={classes.nested}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary='Starred' />
-          </ListItem>
-        </List>
-      </Collapse>
-    </List>
-  );
-};
-
-export default SettingTabs;
+  render() {
+    return (
+      <div style={{ height: 400, width: 400 }}>
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={(treeData) => this.setState({ treeData })}
+        />
+      </div>
+    );
+  }
+}
