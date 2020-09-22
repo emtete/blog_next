c406a100067d53ff0151bd24cfa971a1a39607cc
최종호 <victor_77@naver.com>
2020-09-22 14:20:57 +0900

    .

--- back/app.js
+++ back/app.js
@@ -29,7 +29,7 @@ passportConfig();
 // 순서 중요.
 // credentials -> 서로 다른 도메인 간 쿠키를 전달 할 수 없는데, 그걸 허용해준다.
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(express.static(path.join(__dirname, "uploads")));
+app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(express.json()); // json 데이터를 req.body 안에 넣어준다.
 app.use(express.urlencoded({ extended: true })); // form 데이터, url encoding된 데이터를  req.body에 넣어준다.
 app.use(cookieParser("next_blog_secret"));
