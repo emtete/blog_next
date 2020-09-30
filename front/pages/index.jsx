d30b273d6d243a09ccfe83d999e08dfde4f1d52d
최종호 <victor_77@naver.com>
2020-09-30 12:07:22 +0900

    .

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,44 +1,32 @@
-import Typography from "@material-ui/core/Typography";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
-import { menuStyles } from "../components/layout/styles";
+const getIsArray = (element) => {
+  return Array.isArray(element) && element.length > 0;
+};
 
 const Home = () => {
-  const classes = menuStyles();
+  const router = useRouter();
+  const treeData = useSelector((state) => state.category.treeData);
 
-  return (
-    // <div className={classes.toolbar} />
-    <>
-      <Typography paragraph>
-        Full stack web developer. view my work Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-        facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus
-        in hendrerit gravida rutrum quisque non tellus. Convallis convallis
-        tellus id interdum velit laoreet id donec ultrices. Odio morbi quis
-        commodo odio aenean sed adipiscing. Amet nisl suscipit adipiscing
-        bibendum est ultricies integer quis. Cursus euismod quis viverra nibh
-        cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
-        lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis
-        at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec
-        massa sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-        aliquam ultrices sagittis orci a.
-      </Typography>
-    </>
-  );
+  useEffect(() => {
+    if (treeData[0] && !getIsArray(treeData[0].children)) {
+      router.push(
+        `/${treeData[0].isCard ? "card" : "post"}?categoryId=${
+          treeData[0].id
+        }&categoryName=${treeData[0].title}`
+      );
+    } else if (treeData[0] && getIsArray(treeData[0].children)) {
+      router.push(
+        `/${treeData[0].children[0].isCard ? "card" : "post"}?categoryId=${
+          treeData[0].children[0].id
+        }&categoryName=${treeData[0].children[0].title}`
+      );
+    }
+  }, [treeData]);
+
+  return <></>;
 };
 
 export default Home;
