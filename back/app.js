ab82f7d0cf5efc96575449ed5e741bb36bae876d
최종호 <victor_77@naver.com>
2020-10-04 11:09:46 +0900

    .

--- back/app.js
+++ back/app.js
@@ -28,8 +28,8 @@ db.sequelize
 
 passportConfig();
 
-app.set("trust proxy", 1);
 if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
   app.use(morgan("combined"));
   app.use(hpp());
   app.use(helmet());
