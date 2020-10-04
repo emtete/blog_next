fb912c8a896672f6e687fa86c0bad146d86b01f8
최종호 <victor_77@naver.com>
2020-10-04 10:23:24 +0900

    .

--- back/app.js
+++ back/app.js
@@ -88,6 +88,6 @@ app.use("/post", postRouter);
 app.use("/user", userRouter);
 app.use("/category", categoryRouter);
 
-app.listen(80, () => {
+app.listen(3065, () => {
   console.log("서버 실행 중..");
 });
