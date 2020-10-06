9e46e786872a8a123530706f407610ca4104a5e9
최종호 <victor_77@naver.com>
2020-10-06 12:04:06 +0900

    .

--- front/config/config.js
+++ front/config/config.js
@@ -1,4 +1,5 @@
 export const backUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.dev-life.kr/"
+    ? // ? "https://api.dev-life.kr/"
+      "http://localhost:3065/"
     : "http://localhost:3065/";
