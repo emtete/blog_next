7e99aa4a835ebc88a2204349e4e338d1cf14a265
최종호 <victor_77@naver.com>
2020-10-26 11:13:04 +0900

    .

--- front/scripts/sitemap-posts.js
+++ front/scripts/sitemap-posts.js
@@ -5,22 +5,22 @@ const axios = require("axios");
 
 const getDate = new Date().toISOString();
 
-const fetchUrl = "https://api.dev-life.kr/post/getPostIds";
+const fetchUrl = "https://api.dev-life.kr/post/getList?userId=1";
 const YOUR_AWESOME_DOMAIN = "https://dev-life.kr";
 
 const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
 
 (async () => {
-  const fetchPosts = await fetch(fetchUrl)
-    .then((res) => console.log(res)) //res.json()
-    .catch((err) => console.log(err));
-
-  // const fetchPosts = axios
-  //   .get(fetchUrl, {
-  //     withCredentials: true,
-  //   })
-  //   .then((res) => res.json())
+  // const fetchPosts = await fetch(fetchUrl)
+  //   .then((res) => console.log(res)) //res.json()
   //   .catch((err) => console.log(err));
+
+  const fetchPosts = axios
+    .get(fetchUrl, {
+      withCredentials: true,
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 
   console.log("fetchPosts : ", fetchPosts);
 
