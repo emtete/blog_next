547f6ee0963e9bfee913b87d2f392582eca3ea99
최종호 <victor_77@naver.com>
2020-10-26 11:37:58 +0900

    .

--- front/scripts/sitemap-posts.js
+++ front/scripts/sitemap-posts.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 const getDate = new Date().toISOString();
 
-const fetchUrl = "https://api.dev-life.kr/post/getList?userId=1";
+const fetchUrl = "https://api.dev-life.kr/post/getPostIds";
 const YOUR_AWESOME_DOMAIN = "https://dev-life.kr";
 
 const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
@@ -15,42 +15,42 @@ const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
   //   .then((res) => console.log(res)) //res.json()
   //   .catch((err) => console.log(err));
 
-  const fetchPosts = axios
+  const fetchPosts = await axios
     .get(fetchUrl, {
       withCredentials: true,
     })
-    .then((res) => console.log(res))
+    .then((res) => res.data)
     .catch((err) => console.log(err));
 
   console.log("fetchPosts : ", fetchPosts);
 
-  // const postList = [];
-  // fetchPosts.forEach((post) => postList.push(post.id));
+  const postList = [];
+  fetchPosts.forEach((post) => postList.push(post.id));
 
-  // const postListSitemap = `
-  //   ${postList
-  //     .map((id) => {
-  //       return `
-  //         <url>
-  //           <loc>${`${YOUR_AWESOME_DOMAIN}/post/${id}`}</loc>
-  //           <lastmod>${getDate}</lastmod>
-  //         </url>`;
-  //     })
-  //     .join("")}
-  // `;
+  const postListSitemap = `
+    ${postList
+      .map((id) => {
+        return `
+          <url>
+            <loc>${`${YOUR_AWESOME_DOMAIN}/post/${id}`}</loc>
+            <lastmod>${getDate}</lastmod>
+          </url>`;
+      })
+      .join("")}
+  `;
 
-  // const generatedSitemap = `
-  //   <?xml version="1.0" encoding="UTF-8"?>
-  //   <urlset
-  //     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  //     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  //     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-  //   >
-  //     ${postListSitemap}
-  //   </urlset>
-  // `;
+  const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${postListSitemap}
+    </urlset>
+  `;
 
-  // const formattedSitemap = [formatted(generatedSitemap)];
+  const formattedSitemap = [formatted(generatedSitemap)];
 
-  // fs.writeFileSync("../public/sitemap-posts.xml", formattedSitemap, "utf8");
+  fs.writeFileSync("../public/sitemap-posts.xml", formattedSitemap, "utf8");
 })();
