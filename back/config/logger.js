ee58eb49a6c471fb0a00c230b2ee75979707d69f
최종호 <victor_77@naver.com>
2020-10-27 19:00:37 +0900

    catch bug

---
+++ back/config/logger.js
@@ -0,0 +1,62 @@
+const winston = require("winston");
+const winstonDaily = require("winston-daily-rotate-file");
+const appRoot = require("app-root-path");
+const process = require("process");
+
+const logDir = `${appRoot}/logs`;
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const logformat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  format: combine(
+    label({ label: "DEV LIFE" }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    logformat
+  ),
+  transports: [
+    //
+    new winstonDaily({
+      level: "info",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: "error",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir,
+      filename: `%DATE%.error.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
+  exceptionHandlers: [
+    new winstonDaily({
+      level: "error",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir,
+      filename: `%DATE%.exception.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
+}
+
+module.exports = logger;
