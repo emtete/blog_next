d22f093de1c3eafde4ce5aa9c1b01f743f019a95
최종호 <victor_77@naver.com>
2020-11-06 18:05:15 +0900

    .

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -126,6 +126,7 @@ const Home = (props) => {
   // 로그인 정보가 변경되는 경우
   useEffect(() => {
     console.log("loadMyInfoDone : ", loadMyInfoDone);
+    console.log("isFirst : ", isFirst);
     if (loadMyInfoDone && isFirst) {
       getFirstList();
     }
@@ -189,30 +190,30 @@ const Home = (props) => {
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  async (context) => {
-    const cookie = context.req ? context.req.headers.cookie : "";
-    axios.defaults.headers.Cookie = "";
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   async (context) => {
+//     const cookie = context.req ? context.req.headers.cookie : "";
+//     axios.defaults.headers.Cookie = "";
 
-    if (context.req && cookie) {
-      axios.defaults.headers.Cookie = cookie;
-    }
+//     if (context.req && cookie) {
+//       axios.defaults.headers.Cookie = cookie;
+//     }
 
-    const cookieArr = cookie && cookie.split("; ");
-    let cookieObj = {};
-    for (let i in cookieArr) {
-      cookieObj[cookieArr[i].split("=")[0]] = cookieArr[i].split("=")[1];
-    }
+//     const cookieArr = cookie && cookie.split("; ");
+//     let cookieObj = {};
+//     for (let i in cookieArr) {
+//       cookieObj[cookieArr[i].split("=")[0]] = cookieArr[i].split("=")[1];
+//     }
 
-    const userId = cookieObj.id || 1;
-    const data = await fetcher(`${backUrl}post/getScrollList?userId=${userId}`);
+//     const userId = cookieObj.id || 1;
+//     const data = await fetcher(`${backUrl}post/getScrollList?userId=${userId}`);
 
-    context.store.dispatch({ type: "LOAD_MY_INFO_REQUEST" });
-    context.store.dispatch(END);
-    await context.store.sagaTask.toPromise();
+//     context.store.dispatch({ type: "LOAD_MY_INFO_REQUEST" });
+//     context.store.dispatch(END);
+//     await context.store.sagaTask.toPromise();
 
-    return { props: { data } };
-  }
-);
+//     return { props: { data } };
+//   }
+// );
 
 export default Home;
